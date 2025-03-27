89d58df10c764e178f60fbd4d5a696bca5c871b7
areebalii <areebktk8@gmail.com>
2025-03-26 23:27:40 -0700

    changes in the styling and js

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 let buttonEls = document.querySelectorAll('button');
 let clearEl = document.querySelector('.clear');
 let equalEl = document.querySelector('.equal');
+let deleteEl = document.querySelector('.delete');
 let inputEl = document.querySelector('input');
-// console.log(displayEl);
+
 
 buttonEls.forEach(buttonEl => {
   buttonEl.addEventListener('click', () => {
-   addValueToInput(buttonEl.textContent)   
+    addValueToInput(buttonEl.textContent)
   })
 })
 
 function addValueToInput(value) {
-  try{
-    if(value == "C"){
+  try {
+    if (value == "C") {
       inputEl.value = "";
       return;
-    } 
-    if(value === "="){
-      inputEl.value = eval(inputEl.value)
-      return;
+    } else if (value === "=") {
+      if(inputEl.value.trim() == "") {
+        inputEl.value = ""
+      }else{
+        inputEl.value = eval(inputEl.value)
+      }
+    } else if (value == "DE") {
+      inputEl.value = inputEl.value.slice(0, -1)
+    } else {
+      inputEl.value += value;
     }
-    inputEl.value += value;
- }
- catch{
-  alert("Invalid Error")
- }
+    
+  }
+  catch {
+    alert("Invalid Error")
+  }
 }
